c9b13bc666fecf7d0516459f9844a73a0a75f7f9
jackie <jackie@egenie.cn>
2019-07-04 23:02:27 +0800

    ci

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,11 @@
 module.exports = {
   verbose: true,
   clearMocks: false,
-  collectCoverage: false,
-  reporters: ["default"],
+  collectCoverage: true,
+  collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  reporters: ["default", "jest-junit"],
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   moduleDirectories: ['node_modules'],
   globals: {
@@ -14,6 +17,7 @@ module.exports = {
   },
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
+    "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/object-mock.js",
   },
   testMatch: ['<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)'],
   transform: {
